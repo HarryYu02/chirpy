dae90b74059d33e52f958341ed2e52a3fa3b5dad
Harry Y. <harryyu02@gmail.com>
2026-02-24 18:10:52 -0500

    add middleware and routes to track requests

--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetServerHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	handler := http.NewServeMux()
-	server := http.Server{
+	server := &http.Server{
 		Handler: handler,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
+	apiCfg := &apiConfig{}
+
+	handler.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix("/app/", http.FileServer(http.Dir("."))),
+		),
+	)
+	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	handler.HandleFunc("/metrics", apiCfg.getServerHits)
+	handler.HandleFunc("/reset", apiCfg.resetServerHits)
 
 	server.ListenAndServe()
 }
